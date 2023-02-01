c16db2bf6aad0de44d87342c7531a114cfc765f5
mobindaraie <mobin.daraie@gmail.com>
2023-02-01 22:31:04 +0000

    added vnet

--- Demo 1 - Answers/main.tf
+++ Demo 1 - Answers/main.tf
@@ -9,11 +9,19 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {}
 }
 
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-demo1"
   location = "Australia East"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-demo1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
 }
